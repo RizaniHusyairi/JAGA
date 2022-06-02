bfd4d63ccfce898daf631cd227b0da59d2db9108
RizaniPanda <leo.rizan68@gmail.com>
2022-06-02 14:51:14 +0800

    nyoba firebase authentication

---
+++ app/src/main/java/com/example/jaga/VerifyActivity.kt
@@ -0,0 +1,14 @@
+package com.example.jaga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+    }
+}
